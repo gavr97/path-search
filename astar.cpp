0489edda249b91bfb75458623d6b769dc120b5b4
Alex <alex.gavr97@gmail.com>
2017-01-07 16:47:06 +0300

    Начал писать astar. Есть наброски структуры кода (без классов - вроде не самое удачное решение)

--- astar.cpp
+++ astar.cpp
@@ -1,4 +1,24 @@
 //
 // Created by alexgavr on 07.01.17.
 //
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <string>
+#include "myMap.h"
+#include "astar.h"
 
+void astar(const Task &task, std::vector<std::vector<std::pair<short, short>>> &adjTable)
+{
+    // ... alg
+
+}
+
+void solve(const Task &task)
+{
+    std::vector<std::vector<std::pair<short, short>>>
+            adjTable(task.cntRealRows + 2,
+            std::vector<std::pair<short, short>>(task.cntRealCols + 2, {0, 0}));
+    astar(task, adjTable);
+    // reestablish path with adjTable;
+}
--- astar.h
+++ astar.h
@@ -5,4 +5,25 @@
 #ifndef REPO_ASTAR_H
 #define REPO_ASTAR_H
 
+#include <vector>
+#include <string>
+#include <myMap.h>
+#include "myMap.h"
+
+double heuristicEuclid(unsigned int leftX, unsigned int leftY, unsigned int rightX, unsigned int rightY);
+
+double heuristicDiagonal(unsigned int leftX, unsigned int leftY, unsigned int rightX, unsigned int rightY);
+
+double heuristicChebyshev(unsigned int leftX, unsigned int leftY, unsigned int rightX, unsigned int rightY);
+
+double heuristicManhattan(unsigned int leftX, unsigned int leftY, unsigned int rightX, unsigned int rightY);  // only when allowdiag = 0;
+
+double heuristic(unsigned int leftX, unsigned int leftY, unsigned int rightX, unsigned int rightY, std::string nameHeuristic);
+
+void astar(const Task &task, std::vector<std::vector<std::pair<short, short>>> &adjTable);
+
+void solve(const Task &task);
+
+
+
 #endif //REPO_ASTAR_H
