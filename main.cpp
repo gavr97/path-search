5c4c1a9644c1a353b9ac571a4687f9faf616d924
Alex <alex.gavr97@gmail.com>
2017-01-16 20:51:32 +0300

    list of inputs added in main()

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 #include "astar.h"
 
 int main() {
+    std::vector<char*> list = {"input/3622198.xml", "input/3664933.xml", "input/3839748.xml", "input/4123378.xml",
+                                "input/4226598.xml", "input/4320060.xml"};
+
     int myeResult = 0;
     Task task;
     myeResult = task.myLoad("input/3664933.xml");  // char* ! not std::string
